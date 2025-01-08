ba9c302583bf6a199539a0de4715cf48ef1c99cc
Victor Boleac <victor.boleac@icloud.com>
2025-01-08 15:47:16 +0000

    initial commit

---
+++ src/test/java/com/mangalitsa/litsa/LitsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangalitsa.litsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
